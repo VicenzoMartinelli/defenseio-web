433f9722ea915a4fd2bd3ddea65fd867ad9d0858
Vicenzo Martinelli <vicenzomartinelli@hotmail.com>
2019-10-29 02:15:03 -0300

    a2

--- src/contexts/GeocodeProvider.js
+++ src/contexts/GeocodeProvider.js
@@ -5,8 +5,8 @@ export const GeocodeContext = createContext(Geocode);
 
 const GeocodeProvider = props => {
   useMemo(() => {
-    console.log('key', process.env.GEOCODE_API_KEY)
-    Geocode.setApiKey(process.env.GEOCODE_API_KEY);
+    console.log("key", process.env.REACT_APP_GEOCODE_API_KEY);
+    Geocode.setApiKey(process.env.REACT_APP_GEOCODE_API_KEY);
     Geocode.setLanguage("pt-BR");
     Geocode.setRegion("br");
     Geocode.enableDebug();
